5a57bdd8e31f52c3413192f49befec5ead537b95
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-02 10:57:16 +0200

    Iteration 1 solved

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -7,17 +7,23 @@ describe("Iteration 1 | Getting Started", () => {
       it("should take two arguments", () => {
         expect(add.length).toBe(2);
       });
-  
+      
       it("should return the sum of the two numbers", () => {
         expect(add(1, 2)).toEqual(3);
         expect(add(3, 4)).toEqual(7);
         expect(add(100, 47)).toEqual(147);
       });
-  
+      
       it("should return undefined if any of the arguments is not provided", () => {
         expect(add(1)).toEqual(undefined);
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
+      });
+      
+      it("should return `undefined` if any of the two arguments is not a number", () => {
+        expect(add(2, "4")).toBe(undefined);
+        expect(add("patata", "pikachu")).toBe(undefined);
+        expect(add("5", 10)).toBe(undefined);
       });
     });
   });
